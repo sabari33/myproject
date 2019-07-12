f4110b1519660c42414d5636c3c5a7fb7c599a2f
sabari33 <52820515+sabari33@users.noreply.github.com>
2019-07-12 15:16:29 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter a number");
+    scanf("%d",&a);
+    if(a>=0)
+      printf("positive");
+    elseif(a<0)
+       printf("negative");
+    else(a==0)
+        printf("zero");
+ }
+ return 0;
